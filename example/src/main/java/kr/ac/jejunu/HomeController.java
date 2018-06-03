f99f4e0995af4e406e4e6485125020b763267e4c
hyunwoo <chzhqk1994@gmail.com>
2018-06-03 13:59:26 +0900

    Add Bootstrap

--- example/src/main/java/kr/ac/jejunu/HomeController.java
+++ example/src/main/java/kr/ac/jejunu/HomeController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
-    public String home(Model model){
-        model.addAttribute("greeting", "hello world!");
+    public String home(){
         return "home";
     }
 }
